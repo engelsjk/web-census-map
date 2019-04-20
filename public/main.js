4df7a6fbfd9bcbdd59b98153161f2cb56e4a9a4b
devbot <devbot@The-Last-Question-3.local>
2019-04-20 17:36:25 -0400

    get state map working

--- public/main.js
+++ public/main.js
@@ -7,12 +7,14 @@ var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height);
 
-var url = "https://gist.githubusercontent.com/mbostock/4090846/raw/d534aba169207548a8a3d670c9c2cc719ff05c47/us.json"
+//var url = "https://gist.githubusercontent.com/mbostock/4090846/raw/d534aba169207548a8a3d670c9c2cc719ff05c47/us.json"
+var url = "https://raw.githubusercontent.com/engelsjk/web-census-map/master/data/states.json"
+
 d3.json(url, function(error, topology) {
   if (error) throw error;
   
   console.log("topojson", topology)
-  var geojson = topojson.feature(topology, topology.objects.counties);
+  var geojson = topojson.feature(topology, topology.objects.tl_2018_us_state);
   console.log("geojson", geojson)
 
   svg.selectAll("path")
